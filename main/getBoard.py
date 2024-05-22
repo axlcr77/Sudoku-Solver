88352a122248e4deef6518bb5fa54451af6ed82c
Axel Castro-Romero <axelcr77@byu.edu>
2024-05-22 13:48:47 -0600

    Add ability to get the board from a text file

--- main/getBoard.py
+++ main/getBoard.py
@@ -2,10 +2,32 @@ import TextFiles
 
 file = open("TextFiles/easy1.txt","r")
 file2 = open("TextFiles/easy2.txt","r")
-lines = file.readlines()
-lines2 = file2.readlines()
+lines = file.read()
+lines2 = file2.read()
+
+
+# print(lines)
+# print(lines2)
+
+def getboard(file):
+    arr = [[0] * 9 for _ in range(9)]
+    with open(file, "r") as file2:
+        lines = file2.readlines()
+        rows = [line.strip() for line in lines]
+    row = 0
+    col = 0
+    for i in range(9):
+        currentRow  = rows[i]
+        numbers = currentRow.split(" ")
+        for number in numbers:
+            arr[col][row] = int(number)
+            row += 1
+        row = 0
+        col += 1
+    return arr
+
+newarr = getboard("TextFiles/easy1.txt")
+print(newarr)
+
 file.close()
 file2.close()
-
-print(lines)
-print(lines2)
