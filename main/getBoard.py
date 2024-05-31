3122e174144b15f80d2a7e541c8b2fa217bbb8ea
Axel Castro-Romero <axelcr77@byu.edu>
2024-05-31 14:47:27 -0600

    Add implementation to solve by box

--- main/getBoard.py
+++ main/getBoard.py
@@ -1,3 +1,4 @@
+import numpy as np
 file = open("TextFiles/easy1.txt","r")
 file2 = open("TextFiles/easy2.txt","r")
 lines = file.read()
@@ -7,8 +8,18 @@ class Board:
     def __init__(self, file):
         self.file = file
         self.board = self.getboard(file)
+
     def getboard(self, file):
-        arr = [[0] * 9 for _ in range(9)]
+        arr = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0]])
+        
         with open(file, "r") as file2:
             lines = file2.readlines()
             rows = [line.strip() for line in lines]
--- main/solver.py
+++ main/solver.py
@@ -7,38 +7,88 @@ def solve(file):
     solved = False
     while not solved:
         # count is for counting the number of boxes, rows, or columns
-        count = 0
+        for row in range(9):
+            for col in range(9):
+                solveByBox(row, col, game)
+                # solveByRow(row, col, game)
+                # solveByCol(row, col, game)
 
-        # count updates on its own
-        for count in range(9):
-            solveByBox(count, game)
-
-        for count in range(9):
-            solveByRow(count, game)
-
-        for count in range(9):
-            solveByCol(count, 9)
-
-        solvedCheck(solved, game)
+        solved = solvedCheck(solved, game)
 
     return instance
 
 
-def solveByBox(index, game):
+def solveByBox(row, col, game):
+    if game[row][col] == 0:
+        possibleNumsRow = checkRow(row,game)
+        if len(possibleNumsRow) == 1:
+                game[row][col] = possibleNumsRow[0]
+        else:
+            #Check column
+            possibleNumsCol = checkColumn(col, game)
+            if len(possibleNumsCol) == 1:
+                game[row][col] = possibleNumsCol[0]
+
+
+
+def solveByRow(row, col, game):
+    if game[row][col] == 0:
+        possibleNumsBox = checkBox(row, game)
     pass
 
 
-def solveByRow(index, game):
-    pass
-
-
-def solveByCol(index, game):
+def solveByCol(row, col, game):
     pass
 
 
 def solvedCheck(solved, game):
-    pass
+    solved = True
+    if game.__contains__(0):
+        solved = False
+    return solved
 
 
-arr = solve("TextFiles/easy1.txt")
+def checkRow(index,game):
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    row = game[index]
+    numsToRemove = []
+    for num in nums:
+        if row.__contains__(num):
+            numsToRemove.append(num)
+    if len(numsToRemove) == 9:
+        return []
+    for i in numsToRemove:
+        nums.remove(i)
+    return nums
+
+
+def checkColumn(index, game):
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    col = game[:, index]
+    numsToRemove = []
+    for num in nums:
+        if col.__contains__(num):
+            numsToRemove.append(num)
+    if len(numsToRemove) == 9:
+        return []
+    for i in numsToRemove:
+        nums.remove(i)
+    return nums
+
+
+def checkBox(index, game):
+    box = game[:index+2, :index+2]
+    nums = [1,2,3,4,5,6,7,8,9]
+    numsToremove = []
+    for num in nums:
+        if box.__contains__(num):
+            numsToremove.append(num)
+    if len(numsToremove) == 9:
+        return []
+    for i in numsToremove:
+        nums.remove(i)
+    return nums
+
+
+arr = solve("TextFiles/testSolvebyBox2.txt")
 print(arr.board)
